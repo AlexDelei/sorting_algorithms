d18192f0ff7c16937225f725e85b75e57b241855
AlexDelei <alexdelei125@gmail.com>
2023-12-16 01:10:39 -0800

    Sorting algorithms

--- sort.h
+++ sort.h
@@ -26,4 +26,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
+
+void insertion_sort_list(listint_t **list);
+listint_t* createNode(int data);
 #endif
