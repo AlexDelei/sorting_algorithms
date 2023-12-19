26e9aa3514997ee452f408ceb6f9f96d6034bb5e
AlexDelei <alexdelei125@gmail.com>
2023-12-19 15:11:33 -0800

    Sorting algorithms

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,34 +7,40 @@
 void cocktail_sort_list(listint_t **list)
 {
 	int swapped;
-	listint_t *temp;
+	listint_t *start = *list;
+	listint_t *end = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	do {
 		swapped = 0;
-		for (temp = *list; temp->next != NULL; temp = temp->next)
+		while (start->next != end)
 		{
-			if (temp->n > temp->next->n)
+			if (start->n > start->next->n)
 			{
-				swap_nodes(temp, temp->next);
+				swap_nodes(list, start, start->next);
 				swapped = 1;
 				print_list(*list);
+			}
+			else
+			{
+				start = start->next;
 			}
 		}
 		if (swapped == 0)
 			break;
 
-		swapped = 0;
-
-		for (; temp->prev != NULL; temp = temp->prev)
+		while (start->prev != NULL)
 		{
-			if (temp->n < temp->prev->n)
+			if (start->n < start->prev->n)
 			{
-				swap_nodes(temp->prev, temp);
+				swap_nodes(list, start->prev, start);
 				swapped = 1;
 				print_list(*list);
+				start = start->next;
 			}
+			else
+				start = start->prev;
 		}
 	} while (swapped);
 }
@@ -44,11 +50,18 @@ void cocktail_sort_list(listint_t **list)
  * @node1: First node to be swapped.
  * @node2: Second node to be swapped.
  */
-void swap_nodes(listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	int temp;
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	if (node2->prev != NULL)
+		node2->next->prev = node1;
 
-	temp = node1->n;
-	node1->n = node2->n;
-	node2->n = temp;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+
+	if (node2->prev == NULL)
+		*list = node2;
 }
